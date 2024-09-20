d32f31b8363df0fd5e294658f1b678bd45649051
Jason Lancaster <jplanc@gmail.com>
2024-09-20 00:24:01 -0400

    Separate k8s and tf build triggers

--- terraform/cloudbuild-trigger.tf
+++ terraform/cloudbuild-trigger.tf
@@ -1,8 +1,24 @@
-resource "google_cloudbuild_trigger" "main" {
-  name            = "bitcoind-example-main"
+resource "google_cloudbuild_trigger" "main-terraform" {
+  name            = "bitcoind-example-main-terraform"
   location        = "us-central1"
-  filename        = "cloudbuild-main.yaml"
+  filename        = "cloudbuild-main-terraform.yaml"
   service_account = "projects/bitcoind-example/serviceAccounts/182110884486-compute@developer.gserviceaccount.com"
+  included_files  = ["terraform/**"]
+
+  repository_event_config {
+    repository = "projects/bitcoind-example/locations/us-central1/connections/jplanc/repositories/jplanc-bitcoind-example"
+    push {
+      branch = "main"
+    }
+  }
+}
+
+resource "google_cloudbuild_trigger" "main-k8s" {
+  name            = "bitcoind-example-main-k8s"
+  location        = "us-central1"
+  filename        = "cloudbuild-main-k8s.yaml"
+  service_account = "projects/bitcoind-example/serviceAccounts/182110884486-compute@developer.gserviceaccount.com"
+  included_files  = ["k8s/**", "Dockerfile"]
 
   repository_event_config {
     repository = "projects/bitcoind-example/locations/us-central1/connections/jplanc/repositories/jplanc-bitcoind-example"
